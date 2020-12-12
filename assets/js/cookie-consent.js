847772c32c891250390ee5c8c8be33aba158378e
Raj Nath Sah <raj4u4all@gmail.com>
2020-12-12 22:07:01 +0400

    Create cookie-consent.js

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,17 @@
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#eaf7f7",
+      "text": "#5c7291"
+    },
+    "button": {
+      "background": "#56cbdb",
+      "text": "#ffffff"
+    }
+  },
+  "content": {
+    "href": "https://blog.rajnath.dev/cookieconsent/"
+  }
+});
+</script>
